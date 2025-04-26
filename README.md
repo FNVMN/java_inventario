ee3ed6b79e02fd169f14a83980f337412b435a59
FNVMN <franciscodonascimentovieiradam@gmail.com>
2025-04-26 16:01:58 -0300

    array de produtos testado

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Projeto Inventário Java
+
+##Requisitos do Projeto
+- java ^23.0.2
+- jdk ^23.0.0
+- eclipse
+
+##Configuração do Git
  - git init -> inicia versionamento no projeto
  - git add . -> adiciona os arquivos do projeto no git
  - git commit -m "Mensagem de entrada" -> Entraga as modificações
 feitas nos arquivos adicionados
  - git branch -M main - define a ramificação de entrega 
- - git remote add origin https://github.com/java_inventario.git -> define o endereço remoto
+ - git remote add origin https://github.com/FNVMN/java_inventario.git -> define o endereço remoto
  - git push -u origin main -> envia os arquivose pastas do projeto para a ramificação raiz (main) no github
   
--- src/inventory/ProductTester.java
+++ src/inventory/ProductTester.java
@@ -1,7 +1,9 @@
 package inventory;
 import java.util.Scanner;
 
+
 public class ProductTester {
+	
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -9,21 +11,54 @@ public class ProductTester {
 		String tempName;
 		int tempQtd;
 		double tempPrice;
+		int maxSize = -1;
 		
-		System.out.println("\n\n Digite o nome do produto: ");
-		tempName = in.nextLine();
-		System.out.println("\n\n Digite a quantidade do estoque: ");
-		tempQtd = in.nextInt();
-		System.out.println("\n\n Digite o preço: ");
-		tempPrice = in.nextDouble();
-		System.out.println("\n\n Digite o identificador único: ");
-		tempNumber = in.nextInt();
+
+		
+		//Avisar o usuario ate que ele insira um numero >=0
+		do {
+			try{ 
+				//mostrar para o usuário 
+				System.out.println("Insira o número de produtos que gostaria de adicionar");
+				System.out.println("Insira 0 (zero) se não quiser adicionar produtos");
+				maxSize = in.nextInt();
+				if(maxSize < 0) {
+					System.out.println("Valor inserido esta incorreto");
+				}  
+			}catch(Exception e) {
+				System.out.println(e);
+				in.nextLine();
+			}
+		}	while(maxSize<0);
+
+		if(maxSize == 0) {
+			System.out.println("Nenhum produto requerido para ser registrado");
+		}else {
+			Product[] products = new Product[maxSize];
+			//armazena os produtos
+			for(int i = 0; i < maxSize; i++) {
+				in.nextLine();
+				System.out.println("\n");
+				System.out.println("Digite o nome do produto: ");
+				tempName = in.nextLine();
+				System.out.println("Digite a quantidade do estoque: ");
+				tempQtd = in.nextInt();
+				System.out.println("Digite o preço: ");
+				tempPrice = in.nextDouble();
+				System.out.println("Digite o identificador único: ");
+				tempNumber = in.nextInt();
+				//criar um objeto produto e armazenar as suas propriedades
+				products[i] = new Product(tempNumber,tempName,tempQtd,tempPrice);
+			}
+			
+			//Exibe os valores armazenados
+			for (Product product: products) {
+				System.out.println(product);
+			}
+		}
+		
 		
 		in.close();
-		Product p = new Product(tempNumber,tempName,tempQtd,tempPrice);
-		//p.setActive(false);
-		System.out.println(p);
-
 	}
 
 }
